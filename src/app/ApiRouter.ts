432d40e48fcc566621bc40a4298c8508f5e81f44
Michael Vezina <vezina95@gmail.com>
2020-05-23 14:07:11 -0400

    - Update formula parsing to allow for inversion of formulas, now proposition formulas are consider "inner" formulas rather than being embedded with k/kp/kw

--- src/app/ApiRouter.ts
+++ src/app/ApiRouter.ts
@@ -2,168 +2,167 @@ import * as express from 'express';
 import {CustomDescription} from './models/CustomDescription';
 import {CustomEnvironment} from './models/CustomEnvironment';
 import {Valuation} from './modules/core/models/epistemicmodel/valuation';
-import {ExampleDescription} from './modules/core/models/environment/exampledescription';
-import {ExplicitEpistemicModel} from './modules/core/models/epistemicmodel/explicit-epistemic-model';
+import {AtomicFormula, KFormula, KposFormula, NotFormula} from './modules/core/models/formula/formula';
 
 
 function createRaw(): object {
     return {
-        "initialModel": {
-        "worlds": [
-            {
-                "name": "w1AA_2AA_3EE",
-                "props": [
-                    "1AA",
-                    "2AA",
-                    "3EE"
-                ]
-            },
-            {
-                "name": "w1AA_2AE_3AE",
-                "props": [
-                    "1AA",
-                    "2AE",
-                    "3AE"
-                ]
-            },
-            {
-                "name": "w1AA_2AE_3EE",
-                "props": [
-                    "1AA",
-                    "2AE",
-                    "3EE"
-                ]
-            },
-            {
-                "name": "w1AA_2EE_3AA",
-                "props": [
-                    "1AA",
-                    "2EE",
-                    "3AA"
-                ]
-            },
-            {
-                "name": "w1AA_2EE_3AE",
-                "props": [
-                    "1AA",
-                    "2EE",
-                    "3AE"
-                ]
-            },
-            {
-                "name": "w1AA_2EE_3EE",
-                "props": [
-                    "1AA",
-                    "2EE",
-                    "3EE"
-                ]
-            },
-            {
-                "name": "w1AE_2AA_3AE",
-                "props": [
-                    "1AE",
-                    "2AA",
-                    "3AE"
-                ]
-            },
-            {
-                "name": "w1AE_2AA_3EE",
-                "props": [
-                    "1AE",
-                    "2AA",
-                    "3EE"
-                ]
-            },
-            {
-                "name": "w1AE_2AE_3AA",
-                "props": [
-                    "1AE",
-                    "2AE",
-                    "3AA"
-                ]
-            },
-            {
-                "name": "w1AE_2AE_3AE",
-                "props": [
-                    "1AE",
-                    "2AE",
-                    "3AE"
-                ]
-            },
-            {
-                "name": "w1AE_2AE_3EE",
-                "props": [
-                    "1AE",
-                    "2AE",
-                    "3EE"
-                ]
-            },
-            {
-                "name": "w1AE_2EE_3AA",
-                "props": [
-                    "1AE",
-                    "2EE",
-                    "3AA"
-                ]
-            },
-            {
-                "name": "w1AE_2EE_3AE",
-                "props": [
-                    "1AE",
-                    "2EE",
-                    "3AE"
-                ]
-            },
-            {
-                "name": "w1EE_2AA_3AA",
-                "props": [
-                    "1EE",
-                    "2AA",
-                    "3AA"
-                ]
-            },
-            {
-                "name": "w1EE_2AA_3AE",
-                "props": [
-                    "1EE",
-                    "2AA",
-                    "3AE"
-                ]
-            },
-            {
-                "name": "w1EE_2AA_3EE",
-                "props": [
-                    "1EE",
-                    "2AA",
-                    "3EE"
-                ]
-            },
-            {
-                "name": "w1EE_2AE_3AA",
-                "props": [
-                    "1EE",
-                    "2AE",
-                    "3AA"
-                ]
-            },
-            {
-                "name": "w1EE_2AE_3AE",
-                "props": [
-                    "1EE",
-                    "2AE",
-                    "3AE"
-                ]
-            },
-            {
-                "name": "w1EE_2EE_3AA",
-                "props": [
-                    "1EE",
-                    "2EE",
-                    "3AA"
-                ]
-            }
-        ]
-    }
+        'initialModel': {
+            'worlds': [
+                {
+                    'name': 'w1AA_2AA_3EE',
+                    'props': [
+                        '1AA',
+                        '2AA',
+                        '3EE'
+                    ]
+                },
+                {
+                    'name': 'w1AA_2AE_3AE',
+                    'props': [
+                        '1AA',
+                        '2AE',
+                        '3AE'
+                    ]
+                },
+                {
+                    'name': 'w1AA_2AE_3EE',
+                    'props': [
+                        '1AA',
+                        '2AE',
+                        '3EE'
+                    ]
+                },
+                {
+                    'name': 'w1AA_2EE_3AA',
+                    'props': [
+                        '1AA',
+                        '2EE',
+                        '3AA'
+                    ]
+                },
+                {
+                    'name': 'w1AA_2EE_3AE',
+                    'props': [
+                        '1AA',
+                        '2EE',
+                        '3AE'
+                    ]
+                },
+                {
+                    'name': 'w1AA_2EE_3EE',
+                    'props': [
+                        '1AA',
+                        '2EE',
+                        '3EE'
+                    ]
+                },
+                {
+                    'name': 'w1AE_2AA_3AE',
+                    'props': [
+                        '1AE',
+                        '2AA',
+                        '3AE'
+                    ]
+                },
+                {
+                    'name': 'w1AE_2AA_3EE',
+                    'props': [
+                        '1AE',
+                        '2AA',
+                        '3EE'
+                    ]
+                },
+                {
+                    'name': 'w1AE_2AE_3AA',
+                    'props': [
+                        '1AE',
+                        '2AE',
+                        '3AA'
+                    ]
+                },
+                {
+                    'name': 'w1AE_2AE_3AE',
+                    'props': [
+                        '1AE',
+                        '2AE',
+                        '3AE'
+                    ]
+                },
+                {
+                    'name': 'w1AE_2AE_3EE',
+                    'props': [
+                        '1AE',
+                        '2AE',
+                        '3EE'
+                    ]
+                },
+                {
+                    'name': 'w1AE_2EE_3AA',
+                    'props': [
+                        '1AE',
+                        '2EE',
+                        '3AA'
+                    ]
+                },
+                {
+                    'name': 'w1AE_2EE_3AE',
+                    'props': [
+                        '1AE',
+                        '2EE',
+                        '3AE'
+                    ]
+                },
+                {
+                    'name': 'w1EE_2AA_3AA',
+                    'props': [
+                        '1EE',
+                        '2AA',
+                        '3AA'
+                    ]
+                },
+                {
+                    'name': 'w1EE_2AA_3AE',
+                    'props': [
+                        '1EE',
+                        '2AA',
+                        '3AE'
+                    ]
+                },
+                {
+                    'name': 'w1EE_2AA_3EE',
+                    'props': [
+                        '1EE',
+                        '2AA',
+                        '3EE'
+                    ]
+                },
+                {
+                    'name': 'w1EE_2AE_3AA',
+                    'props': [
+                        '1EE',
+                        '2AE',
+                        '3AA'
+                    ]
+                },
+                {
+                    'name': 'w1EE_2AE_3AE',
+                    'props': [
+                        '1EE',
+                        '2AE',
+                        '3AE'
+                    ]
+                },
+                {
+                    'name': 'w1EE_2EE_3AA',
+                    'props': [
+                        '1EE',
+                        '2EE',
+                        '3AA'
+                    ]
+                }
+            ]
+        }
     };
 }
 
@@ -196,20 +195,22 @@ export class ApiRouter {
 
             // Set the pointed world
             let valuation = new Valuation(props);
-            curEnvironment.getEpistemicModel().setPointedWorld(valuation)
+            curEnvironment.getEpistemicModel().setPointedWorld(valuation);
 
             res.end();
         });
 
 
         app.get('/api/model', async function(req, res) {
-            if (!curEnvironment)
+            if (!curEnvironment) {
                 return res.send({error: 'No Environment.'});
+            }
 
             let resultObject: any = {};
 
-            if (req.query && req.query.description)
+            if (req.query && req.query.description) {
                 resultObject.description = curEnvironment.getExampleDescription();
+            }
 
             resultObject.model = curEnvironment.getEpistemicModel();
 
@@ -224,12 +225,31 @@ export class ApiRouter {
          * Output: { result: boolean }
          */
         app.get('/api/evaluate', async function(req, res) {
-            if (!curEnvironment)
+            if (!curEnvironment) {
                 return res.send({error: 'No Environment.'});
+            }
 
             let formula = req.query.formula || '';
             let result = await curEnvironment.modelCheckFormula(formula);
-            await curEnvironment.getEpistemicModel().obtainKnowledge(CustomDescription.DEFAULT_AGENT)
+            await curEnvironment.getEpistemicModel().obtainKnowledge(CustomDescription.DEFAULT_AGENT);
+
+            res.send({result});
+        });
+
+        /**
+         *
+         */
+        app.post('/api/evaluateFormula', async function(req, res) {
+            if (!curEnvironment) {
+                return res.send({error: 'No Environment.'});
+            }
+
+            let formula = req.body || '';
+
+
+            let parsedFormula = parseFormulaObject(formula);
+
+            let result = await curEnvironment.modelCheckFormula(parsedFormula);
 
             res.send({result});
         });
@@ -242,31 +262,66 @@ export class ApiRouter {
          * Output: { success: boolean }
          */
         app.put('/api/props', async function(req, res) {
-            let propValues: { [id: string]: boolean } | string[] = req.body || [];
+            let propValues: { [id: string]: boolean } | string[] = req.body.props || [];
             let valuation: Valuation = new Valuation(propValues);
 
             let {success, result} = await curEnvironment.updateModel(valuation);
+            let formulaResults: any = {};
+
+            if (success && req.body.formulas) {
+                for (let formula of req.body.formulas) {
+                    let parsedFormula = parseFormulaObject(formula);
+                    formulaResults[formula.id] = result.modelCheck(result.getPointedWorldID(), parsedFormula);
+                }
+            }
 
             // Get any props/knowledge that can be inferred by the world successors
             let inferredProps = result.obtainKnowledge(CustomDescription.DEFAULT_AGENT).getPropositionMap();
             let originalProps = valuation.getPropositionMap();
 
             // Remove any props that were passed in as input.
-            for (let prop of Object.keys(inferredProps))
-            {
-                if(inferredProps[prop] === originalProps[prop])
+            for (let prop of Object.keys(inferredProps)) {
+                if (inferredProps[prop] === originalProps[prop]) {
                     delete inferredProps[prop];
+                }
             }
 
             res.send({
                 success,
-                result: inferredProps
+                result: formulaResults
             });
             res.end();
         });
 
 
+        function parseFormulaObject(formula: any) {
+            if (!formula) {
+                return;
+            }
+
+            let agent = formula.agent || CustomDescription.DEFAULT_AGENT;
+            let type = formula.type;
+            let inverted = formula.invert === undefined ? true : formula.invert;
+
+            // Nested formula (returns undefined if there is no inner formula)
+            let innerFormula = parseFormulaObject(formula.inner);
+            let returnObject;
+
+            if (type === 'knows' || type === 'know' || type === 'k') {
+                returnObject = new KFormula(agent, innerFormula);
+            } else if (type === 'possible') {
+                returnObject = new KposFormula(agent, innerFormula);
+            } else if (type === 'prop') {
+                returnObject = new AtomicFormula(formula.prop);
+            } else {
+                throw 'invalid type ' + type;
+            }
+
+            return inverted ? new NotFormula(returnObject) : returnObject;
+        }
 
 
     }
+
+
 };
